6783e6ace73452191b5dcea0c071722abce0f992
wjkaufman <will@wjkaufman.com>
2018-12-18 17:46:57 -0700

    add save json feature

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Playing with an idea to create helpful maps of knowledge and recommending what to study.
 
 To run locally: `python -m SimpleHTTPServer`.
+
+## TODO
+
+- figure out how to save JSON files
+- add buttons to add nodes, links between nodes
+- add more data to nodes, like topic, when I studied it, etc.
+
+### Bibliography
+
+I've been inspired by the following sources:
+
+- https://beta.observablehq.com/@mbostock/d3-force-directed-graph
+- http://bl.ocks.org/hlvoorhees/9d58e173825aed1e0218
+- https://stackoverflow.com/questions/34156282/how-do-i-save-json-to-local-text-file
+- 
--- force.js
+++ force.js
@@ -1,7 +1,21 @@
-width  = 500
-height = 500
+console.log('hello world! starting up now...');
+
+width  = 600
+height = 600
 
 data = null
+
+// make this a global variable, otherwise it doesn't
+// "remember" the domain that it implicitly learns
+var scale = d3.scaleOrdinal(d3.schemeCategory10);
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([JSON.stringify(content)], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
 
 drag = simulation => {
   
@@ -28,18 +42,17 @@ drag = simulation => {
       .on("end", dragended);
 }
 
-color = d => {
-  const scale = d3.scaleOrdinal(d3.schemeCategory10);
+function color(d) {
   return scale(d.group);
 }
-
-
 
 function forceSimulation(nodes, links) {
   return d3.forceSimulation(nodes)
       .force("link", d3.forceLink(links).id(d => d.id))
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter());
+      // .force("x", d3.forceX())
+      // .force("y", d3.forceY());
 }
 
 const svg = d3.select('body').append('svg')
@@ -47,49 +60,81 @@ const svg = d3.select('body').append('svg')
   .attr('height', height)
   .attr("viewBox", [-width / 2, -height / 2, width, height]);
 
-d3.select('body').append('button')
-
-d3.json("miserables.json")
-  .then(res => {
-    
-    data = res
-    
-    const links = data.links.map(d => Object.create(d));
-    const nodes = data.nodes.map(d => Object.create(d));
-    
-    function ticked() {
-      link
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
+function loadData(dataURL) {
+  console.log('loadData called with URL');
+  
+  // remove old graph
+  d3.selectAll("g").remove()
+  console.log('removed all g elements from svg (I think)');
+  
+  // reset color
+  scale = d3.scaleOrdinal(d3.schemeCategory10);
+  
+  d3.json(dataURL)
+    .then(d => {
       
-      node
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y);
-    }
-    
-    const simulation = forceSimulation(nodes, links).on("tick", ticked);
-    
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-      .selectAll("line")
-      .data(links)
-      .enter().append("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
-    
-    const node = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-      .selectAll("circle")
-      .data(nodes)
-      .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", color)
-        .call(drag(simulation));
-    
-    node.append("title")
-        .text(d => d.id);
-    
+      data = d
+      
+      console.log('allegedly just rewrote data...');
+      
+      const links = data.links.map(d => Object.create(d));
+      const nodes = data.nodes.map(d => Object.create(d));
+      
+      function ticked() {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+        
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      }
+      
+      const simulation = forceSimulation(nodes, links)
+        .on("tick", ticked);
+      
+      const link = svg.append("g")
+          .attr("stroke", "#999")
+          .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .enter().append("line")
+          .attr("stroke-width", d => Math.sqrt(d.value));
+      
+      const node = svg.append("g")
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .enter().append("circle")
+          .attr("r", 5)
+          .attr("fill", color)
+          .call(drag(simulation));
+      
+      node.append("title")
+          .text(d => d.id);
   })
+}
+
+d3.select('input')
+  .on('change', function() {
+    console.log('apparently the file has been changed...');
+    var file = d3.event.target.files[0];
+      if (file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) {
+          var dataUrl = evt.target.result;
+          loadData(dataUrl);
+        };
+       reader.readAsDataURL(file);
+      }
+  })
+
+d3.select('button')
+  .on('click', function() {
+    download(data, 'graph.json', 'text/plain')
+  })
+
+loadData("miserables.json")
