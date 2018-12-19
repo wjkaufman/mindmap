85a38d34b8eb7ccd9bf90f816bf3eea59c541fdf
wjkaufman <will@wjkaufman.com>
2018-12-18 21:26:02 -0700

    separate data loading into different functions

--- force.js
+++ force.js
@@ -60,9 +60,58 @@ const svg = d3.select('body').append('svg')
   .attr('class', 'svg')
   .attr('width', width)
   .attr('height', height)
-  .attr("viewBox", [-width / 2, -height / 2, width, height]);
+  .attr("viewBox", [-width / 2, -height / 2, width, height])
+  .on('dblclick', function() {
+    data.nodes.push({id: "New node", group: 1})
+    
+    // TODO fill this in
+    
+    console.log('double click event');
+    console.log(data.nodes);
+  })
 
-function loadData(dataURL) {
+function loadData(d) {
+  const links = data.links.map(d => Object.create(d));
+  const nodes = data.nodes.map(d => Object.create(d));
+  
+  function ticked() {
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+    
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+  }
+  
+  const simulation = forceSimulation(nodes, links)
+    .on("tick", ticked);
+  
+  const link = svg.append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+      .attr("stroke-width", d => Math.sqrt(d.value));
+  
+  const node = svg.append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+      .attr("r", 5)
+      .attr("fill", color)
+      .call(drag(simulation));
+  
+  node.append("title")
+      .text(d => d.id);
+}
+
+function loadDataURL(dataURL) {
   console.log('loadData called with URL');
   
   // remove old graph
@@ -74,50 +123,9 @@ function loadData(dataURL) {
   
   d3.json(dataURL)
     .then(d => {
-      
       data = d
-      
-      console.log('allegedly just rewrote data...');
-      
-      const links = data.links.map(d => Object.create(d));
-      const nodes = data.nodes.map(d => Object.create(d));
-      
-      function ticked() {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-        
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-      }
-      
-      const simulation = forceSimulation(nodes, links)
-        .on("tick", ticked);
-      
-      const link = svg.append("g")
-          .attr("stroke", "#999")
-          .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .enter().append("line")
-          .attr("stroke-width", d => Math.sqrt(d.value));
-      
-      const node = svg.append("g")
-          .attr("stroke", "#fff")
-          .attr("stroke-width", 1.5)
-        .selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-          .attr("r", 5)
-          .attr("fill", color)
-          .call(drag(simulation));
-      
-      node.append("title")
-          .text(d => d.id);
-  })
+      loadData(d)
+    })
 }
 
 d3.select('input')
@@ -128,7 +136,7 @@ d3.select('input')
         var reader = new FileReader();
         reader.onloadend = function(evt) {
           var dataUrl = evt.target.result;
-          loadData(dataUrl);
+          loadDataURL(dataUrl);
         };
        reader.readAsDataURL(file);
       }
@@ -139,4 +147,4 @@ d3.select('button')
     download(data, 'graph.json', 'text/plain')
   })
 
-loadData("miserables.json")
+loadDataURL("miserables.json")
